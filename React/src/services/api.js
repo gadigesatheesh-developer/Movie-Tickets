905e0b309504859f20a0eff96812627bc9b384f6
satheeh-tech24 <yourEmail@example.com>
2026-02-24 00:15:00 +0530

    first commit

---
+++ React/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "337a1d0507f510ca3621ee7125fdffa2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
